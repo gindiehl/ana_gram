cc5f85bb04d1b60444eca1421052134e2e17e577
jin camou <jincamou@gmail.com>
2017-04-21 15:43:23 -0700

    update renamed project file with passing spec to check regardless of case

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,6 @@
 class String
   define_method :is_anagram? do |input|
     self.downcase.split('').sort == input.downcase.split('').sort
-
   end
 
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,4 +6,7 @@ describe('String#is_anagram?') do
   it('checks if words are anagrams') do
     expect("art".is_anagram?("rat")).to(eq(true))
   end
+  it('checks if words are anagrams regardless of case') do
+    expect("aRt".is_anagram?("rAt")).to(eq(true))
+  end
 end
